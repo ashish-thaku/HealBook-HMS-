e597986b5ccc07f22912d68fbcd62f1b767333e0
Ashish <ashishthaku001@gmail.com>
2026-01-24 22:04:31 +0530

    Fixed header image, updated favicon to logo1.svg

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets_frontend/assets'
 const Header = () => {
   return (
     
-    <div className='flex flex-col md:flex-row flex-wrap bg-primary rounded lg px-6 md:px-10'>
+    <div className='flex flex-col md:flex-row bg-primary rounded-lg px-6 md:px-10 lg:px-20'>
         {/* Left Side */}
         <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
             <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight'>
@@ -20,8 +20,8 @@ const Header = () => {
             </a>
         </div>
         {/* Right Side */}
-        <div className='md:w-1/2 relative'>
-            <img className='w-full md:absolute bottom-0 h-auto rounded-lg' src={assets.header_img} alt="" />
+        <div className='md:w-1/2 flex items-end'>
+            <img className='w-full h-auto rounded-lg' src={assets.header_img} alt="" />
         </div>
     </div>
   )
